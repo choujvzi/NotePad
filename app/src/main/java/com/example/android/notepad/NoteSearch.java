89680cfb23c981e5fae45226a786b77a1db305cb
choujvzi <3291396814@qq.com>
2022-11-19 16:57:40 +0800

    NotePad

---
+++ app/src/main/java/com/example/android/notepad/NoteSearch.java
@@ -0,0 +1,105 @@
+package com.example.android.notepad;
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+public class NoteSearch extends Activity implements SearchView.OnQueryTextListener
+{
+    ListView listView;
+    SQLiteDatabase sqLiteDatabase;
+    /**
+     * The columns needed by the cursor adapter
+     */
+
+    private static final String[] PROJECTION = new String[]{
+            NotePad.Notes._ID, // 0
+            NotePad.Notes.COLUMN_NAME_TITLE, // 1
+            NotePad.Notes.COLUMN_NAME_MODIFICATION_DATE,//时间
+            NotePad.Notes.COLUMN_NAME_BACK_COLOR
+    };
+
+    public boolean onQueryTextSubmit(String query) {
+        Toast.makeText(this, "you choose:"+query, Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note_search);
+        SearchView searchView = findViewById(R.id.search_view);
+        Intent intent = getIntent();
+        if (intent.getData() == null) {
+            intent.setData(NotePad.Notes.CONTENT_URI);
+        }
+        listView = findViewById(R.id.list_view);
+        sqLiteDatabase = new NotePadProvider.DatabaseHelper(this).getReadableDatabase();
+        //Set the searchview to display the search button
+        searchView.setSubmitButtonEnabled(true);
+
+        //Set the prompt text displayed by default in this searchview
+        searchView.setQueryHint("search");
+        searchView.setOnQueryTextListener(this);
+
+    }
+    public boolean onQueryTextChange(String string) {
+        String selection1 = NotePad.Notes.COLUMN_NAME_TITLE+" like ? or "+NotePad.Notes.COLUMN_NAME_NOTE+" like ?";
+        String[] selection2 = {"%"+string+"%","%"+string+"%"};
+        Cursor cursor = sqLiteDatabase.query(
+                NotePad.Notes.TABLE_NAME,
+                PROJECTION, // The columns to return from the query
+                selection1, // The columns for the where clause
+                selection2, // The values for the where clause
+                null,          // don't group the rows
+                null,          // don't filter by row groups
+                NotePad.Notes.DEFAULT_SORT_ORDER // The sort order
+        );
+        // The names of the cursor columns to display in the view, initialized to the title column
+        String[] dataColumns = {
+                NotePad.Notes.COLUMN_NAME_TITLE,
+                NotePad.Notes.COLUMN_NAME_MODIFICATION_DATE
+        } ;
+        // The view IDs that will display the cursor columns, initialized to the TextView in
+        // noteslist_item.xml
+        int[] viewIDs = {
+                android.R.id.text1,
+                android.R.id.text2
+        };
+        // Creates the backing adapter for the ListView.
+        MyCursorAdapter adapter = new MyCursorAdapter(
+                this,
+                R.layout.noteslist_item,
+                cursor,
+                dataColumns,
+                viewIDs
+        );
+        // Sets the ListView's adapter to be the cursor adapter that was just created.
+        listView.setAdapter(adapter);
+        return true;
+    }
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        // Constructs a new URI from the incoming URI and the row ID
+        Uri uri = ContentUris.withAppendedId(getIntent().getData(), id);
+        // Gets the action from the incoming Intent
+        String action = getIntent().getAction();
+        // Handles requests for note data
+        if (Intent.ACTION_PICK.equals(action) || Intent.ACTION_GET_CONTENT.equals(action)) {
+            // Sets the result to return to the component that called this Activity. The
+            // result contains the new URI
+            setResult(RESULT_OK, new Intent().setData(uri));
+        } else {
+            // Sends out an Intent to start an Activity that can handle ACTION_EDIT. The
+            // Intent's data is the note ID URI. The effect is to call NoteEdit.
+            startActivity(new Intent(Intent.ACTION_EDIT, uri));
+        }
+    }
+}
